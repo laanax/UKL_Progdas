5bb261b0a71b8b30dc3e66afd963ddaad9231a11
laanax <dimasszemora@gmail.com>
2024-12-04 21:06:14 +0700

    Add files via upload

---
+++ UKL_Project/sedang2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class sedang2 {
+    static void hitung(int r, int t) {
+        double phi = 3.14;
+        double result =phi * r * r * t;
+        
+        System.out.println("volume tabung: " + result);
+    }
+        
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("masukkan jari jari");
+            int jari= scanner.nextInt();
+            System.out.println("masukkan tinggi");
+            int tinggi= scanner.nextInt();
+            hitung(jari, tinggi);
+        } 
+}
---
+++ UKL_Project/so2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class so2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("masukkan jumlah elemen array");
+        int a = scanner.nextInt();
+        
+        int array[] = new int[a];
+        System.out.print("masukkan " + a + " elemen array");
+        for (int i = 0; i < a; i++) {
+            array[i] = scanner.nextInt();
+        }
+        boolean duplikat= false;
+        boolean adaDuplikat = false;
+        System.out.print("Array memiliki elemen duplikat: ");
+        for (int i = 0; i < a; i++) {
+            for (int j = i + 1; j < a; j++) {
+                if (array[i] == array[j]) {
+                    System.out.print(array[i] + " ");  // Menampilkan elemen duplikat
+                    adaDuplikat = true;
+                    break; 
+                }
+    }
+}
+        if (!adaDuplikat) {
+            System.out.println("Tidak ada item duplikat");
+        }
+    }
+}
